1ec4881885ee7b9421ae67368803400c47305bcb
jessib18 <jessib2009@live.de>
2025-12-18 09:43:02 +0100

    run.sh with proper path for crontab exec

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo docker run --rm -v $(pwd)/persistent_data:/persistent_data enbot
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+echo "$SCRIPT_DIR"
+sudo docker run --rm -v ${SCRIPT_DIR}/persistent_data:/persistent_data enbot
