d77defdd8f902c6a346758b9700b9e5a32e0d766
jessib18 <jessib2009@live.de>
2025-12-20 21:36:27 +0000

    add env in run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "$SCRIPT_DIR"
-sudo docker run --rm -v ${SCRIPT_DIR}/persistent_data:/persistent_data enbot
+sudo docker run --rm -v ${SCRIPT_DIR}/persistent_data:/persistent_data --env-file ${SCRIPT_DIR}/BskyRepostBot/.env enbot
